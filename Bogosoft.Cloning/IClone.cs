a636c8bc2194959f3a153916cd6202b5068d14f5
Clayton Roth <croth0627@yahoo.com>
2018-11-23 12:12:36 -0700

    initial code commit

---
+++ Bogosoft.Cloning/IClone.cs
@@ -0,0 +1,16 @@
+﻿namespace Bogosoft.Cloning
+{
+    /// <summary>
+    /// Represents any type capable of creating exact copies of objects of a specified type.
+    /// </summary>
+    /// <typeparam name="T">The type of the object that can be cloned.</typeparam>
+    public interface IClone<T>
+    {
+        /// <summary>
+        /// Clone a given object, creating an exact copy.
+        /// </summary>
+        /// <param name="obj">An object to be cloned.</param>
+        /// <returns>A new reference to an exact copy of the given object.</returns>
+        T Clone(T obj);
+    }
+}
---
+++ Bogosoft.Cloning/ICloneable.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Bogosoft.Cloning
+{
+    /// <summary>
+    /// Represents any type capable of making exact copies of itself.
+    /// </summary>
+    /// <typeparam name="T">The type of the object that can be cloned.</typeparam>
+    public interface ICloneable<T> : ICloneable
+    {
+        /// <summary>
+        /// Clone the current instance, creating an exact copy of itself.
+        /// </summary>
+        /// <returns>A new reference to an exact copy of the current object.</returns>
+        new T Clone();
+    }
+
+    /// <summary>
+    /// Extended functionality for the <see cref="ICloneable{T}"/> contract.
+    /// </summary>
+    public static class ICloneableExtensions
+    {
+        /// <summary>
+        /// Clone the current instance and invoke a modifier action against the new copy before returning it.
+        /// </summary>
+        /// <typeparam name="T">The type of the object that can be cloned.</typeparam>
+        /// <param name="self">The current <see cref="ICloneable{T}"/> instance.</param>
+        /// <param name="modifier">
+        /// A modifier action to invoke against the new copy before returning it.
+        /// </param>
+        /// <returns>
+        /// A new reference to an exact copy of the current instance and modified by the given modifier action.
+        /// </returns>
+        public static T CloneAnd<T>(this ICloneable<T> self, Action<T> modifier)
+        {
+            var clone = self.Clone();
+
+            modifier.Invoke(clone);
+
+            return clone;
+        }
+    }
+}
